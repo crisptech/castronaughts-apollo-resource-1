a433a5e968ef38d9afada418529f5750138a4962
crisptech <crisptech@hotmail.com>
2022-04-25 15:56:33 +0100

    feat: add necessary types and queries to the graphql schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,6 +2,11 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   # schema defined here
+  type Query {
+    tracksForHome: [Track!]!
+    spaceCats: [SpaceCat!]!
+  }
+
   type Author {
     id: ID!
     name: String!
@@ -16,6 +21,23 @@ const typeDefs = gql`
     length: Int
     modulesCount: Int
   }
+
+  type SpaceCat {
+    id: ID!
+    name: String!
+    age: Int
+    missions: Mission
+  }
+
+  type Mission {
+    id: ID!
+    name: String!
+    description: String!
+  }
 `;
+
+/**
+ * Create a full schema with: a type Query containing a field spaceCats to fetch a List of SpaceCat. A type SpaceCat with its subfields: id of type ID!, name of type String!, age of type Int and missions of type List of Mission. Finally define the Mission type with its subfields: id of type ID!, name of type String!, and description of type String!.
+ */
 
 module.exports = typeDefs;
